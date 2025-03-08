8cda5925084571051064a8fb76f6ab37a4807902
Carlos Evens <carlos.evens02@gmail.com>
2025-03-08 13:01:46 -0300

    Inicializacion del proyecto

---
+++ app.js
@@ -0,0 +1,7 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let lista_amigos=[];
+lista_amigos.push("Maria");
+function agregarAmigo(){
+
+    console.log(lista_amigos);
+}
