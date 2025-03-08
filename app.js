07496e26b2deb76064d1cfa6b01c582f47da8982
Carlos Evens <carlos.evens02@gmail.com>
2025-03-08 13:15:19 -0300

    Funcion para actualizar amigos

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 lista_amigos=["Juan","Pedro","Maria","Ana","Luis","Pablo","Jose","Sofia","Laura","Isabel"];
 console.log(lista_amigos);
+actualizarAmigos();
 function agregarAmigo(){
-    let amigoAgregado=document.getElementById("amigo").value;
+    let amigoAgregado=document.querySelector("#amigo").value;
     lista_amigos.push(amigoAgregado);
     console.log(lista_amigos);
+    actualizarAmigos();
+}
+function actualizarAmigos(){
+let ul_amigos=document.querySelector("#listaAmigos");
+ul_amigos.innerHTML="";
+for(let i=0;i<lista_amigos.length;i++){
+    let li_amigo=document.createElement("li");
+    li_amigo.textContent=lista_amigos[i];
+    ul_amigos.appendChild(li_amigo);
+
+}
+
+
 }
