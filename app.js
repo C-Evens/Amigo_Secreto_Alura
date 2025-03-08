bb82df73d70915af5e549c03f87ecf8028f94ee7
Carlos Evens <carlos.evens02@gmail.com>
2025-03-08 13:20:49 -0300

    Proyecto Finalizado

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-lista_amigos=["Juan","Pedro","Maria","Ana","Luis","Pablo","Jose","Sofia","Laura","Isabel"];
-console.log(lista_amigos);
+lista_amigos=[];
 actualizarAmigos();
 function agregarAmigo(){
     let amigoAgregado=document.querySelector("#amigo").value;
