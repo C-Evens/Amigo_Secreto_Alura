9d259a2f730b6ecd7066758990c31d8c10144cdb
Carlos Evens <carlos.evens02@gmail.com>
2025-03-08 13:07:11 -0300

    Funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 lista_amigos=["Juan","Pedro","Maria","Ana","Luis","Pablo","Jose","Sofia","Laura","Isabel"];
 console.log(lista_amigos);
+function agregarAmigo(){
+    let amigoAgregado=document.getElementById("amigo").value;
+    lista_amigos.push(amigoAgregado);
+    console.log(lista_amigos);
+}
